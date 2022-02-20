ea0782e74b628e57a8bde18f1ef665a43b5aa014
Christopher Barnes <chris.barnes.2000@me.com>
2022-02-19 17:28:34 -0800

    [Add]: Stripe Components

---
+++ components/stripe/Cart.tsx
@@ -0,0 +1,14 @@
+import React, { ReactNode } from 'react'
+import { CartProvider } from 'use-shopping-cart/react'
+import config from '../../react-bricks/config'
+
+const Cart = ({ children }: { children: ReactNode }) => (
+  <CartProvider
+    cartMode="checkout-session"
+    stripe={process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string}
+    currency={config.CURRENCY}>
+    <>{children}</>
+  </CartProvider>
+)
+
+export default Cart
